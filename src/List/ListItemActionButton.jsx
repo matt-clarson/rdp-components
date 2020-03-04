be3728b0ec2a86f1a9f5d649e070b39b879f3f61
matt-clarson <clarson469@googlemail.com>
2020-03-04 13:57:50 +0000

    added actions story to list

--- src/List/ListItemActionButton.jsx
+++ src/List/ListItemActionButton.jsx
@@ -4,20 +4,27 @@ import ComponentFactory from "../utils/ComponentFactory";
 import Icon from "../Icon";
 import { css } from "./List";
 
-const ListItemActionButton = ({ icon, onClick, children, ...baseProps }) => (
+const ListItemActionButton = ({
+  title,
+  icon,
+  onClick,
+  children,
+  ...baseProps
+}) => (
   <ComponentFactory
     defaultTag="button"
     fixedClassName={css.elem`item-action-button`}
     {...{ onClick, ...baseProps }}
   >
-    {icon && <Icon icon={icon} />}
+    {icon && <Icon icon={icon} title={title} />}
     {children}
   </ComponentFactory>
 );
 
 ListItemActionButton.propTypes = {
+  title: PropTypes.string,
   icon: PropTypes.string,
-  onClick: PropTypes.func.isRequired,
+  onClick: PropTypes.func,
   children: PropTypes.node
 };
 
--- src/List/stories.js
+++ src/List/stories.js
@@ -23,31 +23,36 @@ const detailedListData = [
     id: 1,
     type: "elm",
     detail:
-      "The elm tree can grow to great height, often with a forked trunk creating a vase profile"
+      "The elm tree can grow to great height, often with a forked trunk creating a vase profile",
+    wikiLink: "https://en.wikipedia.org/wiki/Elm"
   },
   {
     id: 2,
     type: "birch",
     detail:
-      "Birch species are generally small to medium-sized trees or shrubs, mostly of northern temperate and boreal climates"
+      "Birch species are generally small to medium-sized trees or shrubs, mostly of northern temperate and boreal climates",
+    wikiLink: "https://en.wikipedia.org/wiki/Birch"
   },
   {
     id: 3,
     type: "oak",
     detail:
-      "Oaks have spirally arranged leaves, with lobate margins in many species; some have serrated leaves or entire leaves with smooth margins"
+      "Oaks have spirally arranged leaves, with lobate margins in many species; some have serrated leaves or entire leaves with smooth margins",
+    wikiLink: "https://en.wikipedia.org/wiki/Oak"
   },
   {
     id: 4,
     type: "spruce",
     detail:
-      "Spruces are large trees, from about 20–60 m (about 60–200 ft) tall when mature, and have whorled branches and conical form"
+      "Spruces are large trees, from about 20–60 m (about 60–200 ft) tall when mature, and have whorled branches and conical form",
+    wikiLink: "https://en.wikipedia.org/wiki/Spruce"
   },
   {
     id: 5,
     type: "pine",
     detail:
-      "Pine trees are evergreen, coniferous resinous trees (or, rarely, shrubs) growing 3–80 m (10–260 ft) tall, with the majority of species reaching 15–45 m (50–150 ft) tall"
+      "Pine trees are evergreen, coniferous resinous trees (or, rarely, shrubs) growing 3–80 m (10–260 ft) tall, with the majority of species reaching 15–45 m (50–150 ft) tall",
+    wikiLink: "https://en.wikipedia.org/wiki/Pine"
   }
 ];
 
@@ -57,6 +62,23 @@ export const detailedList = () => (
       <ListItem key={id}>
         <ListItemText>{type}</ListItemText>
         <ListItemText secondary>{detail}</ListItemText>
+      </ListItem>
+    ))}
+  </List>
+);
+
+export const withAction = () => (
+  <List>
+    {detailedListData.map(({ id, type, detail, wikiLink = "#" }) => (
+      <ListItem key={id}>
+        <ListItemText>{type}</ListItemText>
+        <ListItemText secondary>{detail}</ListItemText>
+        <ListItemActionButton
+          tag="a"
+          href={wikiLink}
+          title={wikiLink}
+          icon="launch"
+        />
       </ListItem>
     ))}
   </List>
